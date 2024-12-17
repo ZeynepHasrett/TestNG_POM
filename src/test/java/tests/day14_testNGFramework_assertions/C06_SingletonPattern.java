47e69e3df51c50bb6e7c3a1c4a27d689b1861fc0
ZeynepHasrett <158554030+ZeynepHasrett@users.noreply.github.com>
2024-12-17 14:47:47 +0300

    day15

--- src/test/java/tests/day14_testNGFramework_assertions/C06_SingletonPattern.java
+++ src/test/java/tests/day14_testNGFramework_assertions/C06_SingletonPattern.java
@@ -2,10 +2,10 @@ package tests.day14_testNGFramework_assertions;
 
 import org.testng.annotations.Test;
 import utilities.ConfigReader;
-
-import java.sql.Driver;
+import utilities.Driver;
 
 public class C06_SingletonPattern {
+
     /*
         Page Object Model (POM)'i dizayn edenler
         Driver class'indan getDriver() yerine farkli kullanimlarin onune gecmek icin
@@ -17,12 +17,22 @@ public class C06_SingletonPattern {
 
         Singleton Pattern
         obje olusturmayi engellemek icin
+        Constructor'i gorunur yapip
+        kimsenin erisememesi icin private isaretliyoruz
      */
 
     @Test
     public void test01() {
-        // testotomasyonu anasayfaya gidin
 
+        // testotomasyon anasayfaya gidin
+        // Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+
+        //Driver driver = new Driver();
+        //driver.driver.get(ConfigReader.getProperty("toUrl"));
+
+        //Driver.driver.get(ConfigReader.getProperty("toUrl"));
+
+        Driver.quitDriver();
 
     }
 
--- src/test/java/tests/day15_testNGAssertions/C02_SoftAssertion.java
+++ src/test/java/tests/day15_testNGAssertions/C02_SoftAssertion.java
@@ -2,14 +2,16 @@ package tests.day15_testNGAssertions;
 
 import org.testng.annotations.Test;
 
-public class C01_CokluAssertion {
+public class C02_SoftAssertion {
 
     @Test
-    public void ilkUrunIsimTesti() {
+    public void softAssertionTesti(){
+
         // testotomasyonu anasayfaya gidin
 
 
         // url'in testotomasyonu icerdigini test edin
+
 
 
         // phone icin arama yapin
